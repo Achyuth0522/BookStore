7f8c97d12eff2ac40db19f7d48596d2aec518526
achu0522 <achyuth.kowligi@ecoprism.com>
2024-03-21 16:33:19 +0530

    Connected to SQL Database.Created and added Categories table to db using migration command.

---
+++ BookStoreApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStoreApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApplication.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
