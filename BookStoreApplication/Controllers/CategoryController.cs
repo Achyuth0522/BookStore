9a5c79743fbabb9caa0bf6a9c7d54af783459412
achu0522 <achyuth.kowligi@ecoprism.com>
2024-03-26 15:52:08 +0530

    Added Client and Server side validations and CRUD operations for records.

--- BookStoreApplication/Controllers/CategoryController.cs
+++ BookStoreApplication/Controllers/CategoryController.cs
@@ -26,9 +26,79 @@ namespace BookStoreApplication.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name","Display Order cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+           
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
+
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+           
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        { 
+            Category? obj = _db.Categories.Find(id); 
+            if (obj == null) 
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
             _db.SaveChanges();
-            return RedirectToAction("Index","Category");
+            return RedirectToAction("Index", "Category");
+
         }
     }
 }
