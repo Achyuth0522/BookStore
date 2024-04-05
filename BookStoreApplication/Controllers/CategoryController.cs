764526afa9ec8f6ddd6861b33f5165016d1443f3
achu0522 <achyuth.kowligi@ecoprism.com>
2024-04-05 11:13:20 +0530

    Added toastr notifications for CRUD operations.

--- BookStoreApplication/Controllers/CategoryController.cs
+++ BookStoreApplication/Controllers/CategoryController.cs
@@ -34,6 +34,7 @@ namespace BookStoreApplication.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index", "Category");
             }
             return View();
@@ -65,6 +66,7 @@ namespace BookStoreApplication.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index", "Category");
             }
             return View();
@@ -97,6 +99,7 @@ namespace BookStoreApplication.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index", "Category");
 
         }
