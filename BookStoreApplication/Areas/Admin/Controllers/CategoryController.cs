71324d155be8df45e6a0a4112f3c5eee468adfed
achu0522 <achyuth.kowligi@ecoprism.com>
2024-04-09 16:38:51 +0530

    Created Areas for Admin and Customer and moved all the data respectively and also separated all the folders using Interfaces.

--- BookStoreApplication/Areas/Admin/Controllers/CategoryController.cs
+++ BookStoreApplication/Areas/Admin/Controllers/CategoryController.cs
@@ -1,20 +1,22 @@
-﻿using BookStoreApplication.Data;
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStoreApplication.Data;
 using BookStoreApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookStoreApplication.Controllers
+namespace BookStoreApplication.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db) 
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
         {
-            _db = db;
-        
+            _unitOfWork = unitOfWork;
+
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList =  _db.Categories.ToList();
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
             return View(objCategoryList);
         }
 
@@ -26,28 +28,28 @@ namespace BookStoreApplication.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if(obj.Name == obj.DisplayOrder.ToString())
+            if (obj.Name == obj.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("name","Display Order cannot exactly match the Name");
+                ModelState.AddModelError("name", "Display Order cannot exactly match the Name");
             }
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
-                _db.SaveChanges();
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index", "Category");
             }
             return View();
-           
+
         }
 
         public IActionResult Edit(int? id)
         {
-            if(id == null || id == 0)
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.Find(id);
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
             //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(c => c.Id == id);
             //Category? categoryFromDb2 = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
 
@@ -64,8 +66,8 @@ namespace BookStoreApplication.Controllers
         {
             if (ModelState.IsValid)
             {
-                _db.Categories.Update(obj);
-                _db.SaveChanges();
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index", "Category");
             }
@@ -79,8 +81,8 @@ namespace BookStoreApplication.Controllers
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.Find(id);
-           
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+
 
             if (categoryFromDb == null)
             {
@@ -89,16 +91,16 @@ namespace BookStoreApplication.Controllers
             return View(categoryFromDb);
         }
 
-        [HttpPost,ActionName("Delete")]
+        [HttpPost, ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
-        { 
-            Category? obj = _db.Categories.Find(id); 
-            if (obj == null) 
+        {
+            Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
+            if (obj == null)
             {
                 return NotFound();
             }
-            _db.Categories.Remove(obj);
-            _db.SaveChanges();
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index", "Category");
 
--- BookStoreApplication/Areas/Customer/Controllers/HomeController.cs
+++ BookStoreApplication/Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BookStoreApplication.Controllers
+namespace BookStoreApplication.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
