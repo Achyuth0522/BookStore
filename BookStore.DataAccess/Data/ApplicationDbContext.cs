359890b12c9a6308b9c26031e0501cbc32e66d40
achu0522 <achyuth.kowligi@ecoprism.com>
2024-04-12 14:07:08 +0530

    Scaffolded Identity for the project.

--- BookStore.DataAccess/Data/ApplicationDbContext.cs
+++ BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿//using BookStore.Models;
 using BookStoreApplication.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookStoreApplication.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -16,6 +18,8 @@ namespace BookStoreApplication.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1},
                 new Category { Id = 2, Name = "Comedy", DisplayOrder = 2 },
